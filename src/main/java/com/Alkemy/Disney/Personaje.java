71457027dd71ab7e8adf8d562093d8912631f452
Nelson <nelgonzalez88@gmail.com>
2021-12-13 19:15:30 -0300

    agrego personaje al proyecto

---
+++ src/main/java/com/Alkemy/Disney/Personaje.java
@@ -0,0 +1,11 @@
+package com.Alkemy.Disney;
+
+public class Personaje {
+    String nombre;
+    int edad;
+
+    public Personaje(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+}
